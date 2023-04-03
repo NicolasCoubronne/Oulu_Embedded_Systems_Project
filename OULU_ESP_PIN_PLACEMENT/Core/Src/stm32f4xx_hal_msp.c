618126c488e0ef05522f6eef16eed79eb95e2ca4
Nicolas COUBRONNE <nicolas.coubronne@ensea.fr>
2023-04-03 20:05:38 +0300

    change in nucleo pin

--- OULU_ESP_PIN_PLACEMENT/Core/Src/stm32f4xx_hal_msp.c
+++ OULU_ESP_PIN_PLACEMENT/Core/Src/stm32f4xx_hal_msp.c
@@ -159,7 +159,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
     __HAL_RCC_GPIOB_CLK_ENABLE();
     /**I2C1 GPIO Configuration
     PB6     ------> I2C1_SCL
-    PB7     ------> I2C1_SDA
+    PB9     ------> I2C1_SDA
     */
     GPIO_InitStruct.Pin = TOF1_I2C_SCL_Pin|TOF1_I2C_SDA_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
@@ -227,7 +227,7 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
 
     /**I2C1 GPIO Configuration
     PB6     ------> I2C1_SCL
-    PB7     ------> I2C1_SDA
+    PB9     ------> I2C1_SDA
     */
     HAL_GPIO_DeInit(TOF1_I2C_SCL_GPIO_Port, TOF1_I2C_SCL_Pin);
 
@@ -352,7 +352,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     PA9     ------> USART1_TX
     PA10     ------> USART1_RX
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+    GPIO_InitStruct.Pin = BT_TX_Pin|BT_RX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -394,9 +394,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     /* Peripheral clock enable */
     __HAL_RCC_USART3_CLK_ENABLE();
 
-    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
     /**USART3 GPIO Configuration
-    PB10     ------> USART3_TX
+    PC10     ------> USART3_TX
     */
     GPIO_InitStruct.Pin = USART_TX_BASE_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
@@ -432,7 +432,7 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     PA9     ------> USART1_TX
     PA10     ------> USART1_RX
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9|GPIO_PIN_10);
+    HAL_GPIO_DeInit(GPIOA, BT_TX_Pin|BT_RX_Pin);
 
   /* USER CODE BEGIN USART1_MspDeInit 1 */
 
@@ -464,7 +464,7 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     __HAL_RCC_USART3_CLK_DISABLE();
 
     /**USART3 GPIO Configuration
-    PB10     ------> USART3_TX
+    PC10     ------> USART3_TX
     */
     HAL_GPIO_DeInit(USART_TX_BASE_GPIO_Port, USART_TX_BASE_Pin);
 
