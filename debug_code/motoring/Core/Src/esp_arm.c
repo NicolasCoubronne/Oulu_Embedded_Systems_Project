b55bb2d52b7a7bb90d8615a86e6a9b8a0bab1a4b
Jussi Virtanen <virtajus@student.oulu.fi>
2023-04-26 18:47:14 +0300

    add base and claw angles offset, caused by height offset

--- debug_code/motoring/Core/Src/esp_arm.c
+++ debug_code/motoring/Core/Src/esp_arm.c
@@ -26,24 +26,30 @@ void arm_angles_from_dist(unsigned int distance,
 		unsigned int *angle_base_joint, unsigned int *angle_middle_joint, unsigned int *angle_claw_joint)
 {
 	// Everything in mm
-	double angle_base_rad, angle_joint_rad, angle_claw_rad;
+	double angle_base_joint_rad, angle_middle_joint_rad, angle_claw_joint_rad;
+	double height_offset;
+	double base_offset_angle, claw_offset_angle;
 	double arm_len_from_base = 154; // a in law of cosines
 	double arm_len_from_joint = 140; // b in law of cosines
 	/* Need some height offset, basically a good estimation could be:
 	 * base height - claw height + object height
 	 */
-	double height_offset = 50;
+	height_offset = 50.0;
 
 	double dist = sqrt(pow((double)distance, 2) - pow(height_offset, 2)); // c in law of cosines
 
 	// beta in law of cosines
-	angle_base_rad = acos((pow(arm_len_from_base, 2) + pow(dist, 2) - pow(arm_len_from_joint, 2)) / (2.0 * arm_len_from_base * dist));
+	angle_base_joint_rad = acos((pow(arm_len_from_base, 2) + pow(dist, 2) - pow(arm_len_from_joint, 2)) / (2.0 * arm_len_from_base * dist));
 	// gamma in law of cosines
-	angle_joint_rad = acos((pow(arm_len_from_base, 2) + pow(arm_len_from_joint, 2) - pow(dist, 2)) / (2.0 * arm_len_from_base * arm_len_from_joint));
+	angle_middle_joint_rad = acos((pow(arm_len_from_base, 2) + pow(arm_len_from_joint, 2) - pow(dist, 2)) / (2.0 * arm_len_from_base * arm_len_from_joint));
 	// alpha in law of cosines
-	angle_claw_rad = M_PI - angle_base_rad - angle_joint_rad;
+	angle_claw_joint_rad = M_PI - angle_base_rad - angle_joint_rad;
 
-	*angle_base_joint = 512 + rad_to_ax(M_PI/2.0 - angle_base_rad);
-	*angle_middle_joint = 512 + rad_to_ax(M_PI - angle_joint_rad);
-	*angle_claw_joint = 512 - rad_to_ax(M_PI/2.0 - angle_claw_rad);
+	// Servo angles will need to be offset due to height offset
+	base_offset_angle = asin(height_offset / distance);
+	claw_offset_angle = acos(height_offset / distance);
+
+	*angle_base_joint = 512 + rad_to_ax(M_PI/2.0 - angle_base_joint_rad - base_offset_angle);
+	*angle_middle_joint = 512 + rad_to_ax(M_PI - angle_middle_joint_rad);
+	*angle_claw_joint = 512 - rad_to_ax(M_PI - angle_claw_joint_rad - claw_offset_angle);
 }
