add496d79e50f4a29597094cbe33b7e14f7b5873
Jussi Virtanen <virtajus@student.oulu.fi>
2023-04-26 18:59:56 +0300

    make sure to use doubles

--- debug_code/motoring/Core/Src/esp_arm.c
+++ debug_code/motoring/Core/Src/esp_arm.c
@@ -25,23 +25,25 @@ unsigned int rad_to_ax(double angle)
 void arm_angles_from_dist(unsigned int distance,
 		unsigned int *angle_base_joint, unsigned int *angle_middle_joint, unsigned int *angle_claw_joint)
 {
-	// Everything in mm
+	/* Calculations in rads and millimeters,
+	 * return values in raw servo angles
+	 */
 	double angle_base_joint_rad, angle_middle_joint_rad, angle_claw_joint_rad;
 	double height_offset;
 	double base_offset_angle, claw_offset_angle;
-	double arm_len_from_base = 154; // a in law of cosines
-	double arm_len_from_joint = 140; // b in law of cosines
+	double arm_len_from_base = 154.0; // a in law of cosines
+	double arm_len_from_joint = 140.0; // b in law of cosines
 	/* Need some height offset, basically a good estimation could be:
 	 * base height - claw height + object height
 	 */
 	height_offset = 50.0;
 
-	double dist = sqrt(pow((double)distance, 2) - pow(height_offset, 2)); // c in law of cosines
+	double dist = sqrt(pow((double)distance, 2.0) - pow(height_offset, 2.0)); // c in law of cosines
 
 	// beta in law of cosines
-	angle_base_joint_rad = acos((pow(arm_len_from_base, 2) + pow(dist, 2) - pow(arm_len_from_joint, 2)) / (2.0 * arm_len_from_base * dist));
+	angle_base_joint_rad = acos((pow(arm_len_from_base, 2.0) + pow(dist, 2.0) - pow(arm_len_from_joint, 2.0)) / (2.0 * arm_len_from_base * dist));
 	// gamma in law of cosines
-	angle_middle_joint_rad = acos((pow(arm_len_from_base, 2) + pow(arm_len_from_joint, 2) - pow(dist, 2)) / (2.0 * arm_len_from_base * arm_len_from_joint));
+	angle_middle_joint_rad = acos((pow(arm_len_from_base, 2.0) + pow(arm_len_from_joint, 2.0) - pow(dist, 2.0)) / (2.0 * arm_len_from_base * arm_len_from_joint));
 	// alpha in law of cosines
 	angle_claw_joint_rad = M_PI - angle_base_rad - angle_joint_rad;
 
